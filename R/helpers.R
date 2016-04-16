7175b306762eb5606aa77972b98883b2a4879adc
Jeff Hughes <jeff.hughes@gmail.com>
2016-04-16 15:34:09 -0400

    Add some small helper functions, and update documentation.

--- R/helpers.R
+++ R/helpers.R
@@ -1,3 +1,90 @@
+#' Return results of a power simulation.
+#'
+#' \code{results} is a generic function that extracts the raw data from a power
+#' simulation.
+#'
+#' @param sim A power simulation object.
+#' @param ... Additional arguments to be passed to the particular method for the
+#'   given object tye.
+#' @return The form of the value returned by \code{results} depends on the class
+#'   of its argument. See the documentation of the particular methods for
+#'   details of what is produced by that method.
+#' @seealso \code{\link{results.pwr_sim}}
+#' @export
+results <- function(sim, ...) UseMethod('results')
+
+
+#' Return results of a power simulation.
+#'
+#' \code{results.pwr_sim} returns the raw data from a power simulation.
+#'
+#' @param sim A power simulation object of type 'pwr_sim'.
+#' @return Returns a data frame with all the data returned from each simulation.
+#' @export
+results.pwr_sim <- function(sim) {
+    return(sim$results)
+}
+
+
+#' Return the tests performed by a power simulation.
+#'
+#' \code{tests} is a generic function that extracts information about the set of
+#' specific tests (parameter values) for a power simulation.
+#'
+#' @param sim A power simulation object.
+#' @param ... Additional arguments to be passed to the particular method for the
+#'   given object tye.
+#' @return The form of the value returned by \code{tests} depends on the class
+#'   of its argument. See the documentation of the particular methods for
+#'   details of what is produced by that method.
+#' @seealso \code{\link{tests.pwr_sim}}
+#' @export
+tests <- function(sim, ...) UseMethod('tests')
+
+
+#' Return the tests performed by a power simulation.
+#'
+#' \code{results.pwr_sim} extracts information about the set of specific tests
+#' (parameter values) for a power simulation.
+#'
+#' @param sim A power simulation object of type 'pwr_sim'.
+#' @return Returns a data frame with one row for each set of simulations that
+#'   was performed.
+#' @export
+tests.pwr_sim <- function(sim) {
+    return(sim$tests)
+}
+
+
+#' Return the number of simulations performed by a power simulation.
+#'
+#' \code{n.sims} is a generic function that extracts information about the
+#' number of simulations (per specific test) performed by a power simulation.
+#'
+#' @param sim A power simulation object.
+#' @param ... Additional arguments to be passed to the particular method for the
+#'   given object tye.
+#' @return The form of the value returned by \code{n.sims} depends on the class
+#'   of its argument. See the documentation of the particular methods for
+#'   details of what is produced by that method.
+#' @seealso \code{\link{n.sims.pwr_sim}}
+#' @export
+n.sims <- function(sim, ...) UseMethod('n.sims')
+
+
+#' Return the number of simulations performed by a power simulation.
+#'
+#' \code{n.sims.pwr_sim} extracts information about the number of simulations
+#' (per specific test) performed by a power simulation.
+#'
+#' @param sim A power simulation object of type 'pwr_sim'.
+#' @return Returns the number of simulations done in each test.
+#' @export
+n.sims.pwr_sim <- function(sim) {
+    return(sim$n.sims)
+}
+
+
 #' Calculate error variance given model coefficients.
 #'
 #' \code{lm_error_var} will calculate the required error variance for a linear
